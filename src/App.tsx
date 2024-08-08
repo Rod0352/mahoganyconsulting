be8674ddbae62d2256c487d70373bdaedc583e8a
Rod0352 <rscott@westell.com>
2024-08-08 16:07:52 -0500

    added some basic start stuff, removed uneeded react biolerplate stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+const button = {
+  backgroundColor: "#438e5d",
+  color: "white",
+  padding: "5px 10px",
+  borderRadius: "4.5px",
+  border: "none",
+};
+
+const header = () => {
+  return (
+    <div 
+    style={{
+      color : "#438e5d",
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'center',
+      height: '100px',
+      fontFamily: 'Arial',
+      fontSize: '24px',
+
+    }}
+    >
+      <h1>Mahogany Hall </h1>
+      <button
+      style={{
+        ...button,
+      }}
+      >About</button>
+      <button
+      style={{
+        ...button,
+      }}
+      >Services</button>
+      <button
+      style={{
+        ...button
+      }}
+      >Gallery</button>
+      <button
+      style={{
+        ...button
+      }}
+      >Contact</button>
+    </div>
+  )
+}
 
 function App() {
+  // #438e5d
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+    style={{
+      backgroundColor: "#fffefc",
+      color: "#438e5d",
+      display: 'flex',
+      flexDirection: 'column',
+      height: '100vh',
+    }}
+    >
+    {header()}
+    <div
+    style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh',
+      fontFamily: 'Arial',
+      fontSize: '24px',
+    }}
+    >
+      <div style={{
+        border: '1px solid #438e5d',
+        width : '50%',
+        height: '50%',
+      }}>
+
+      </div>
+    </div>
     </div>
   );
 }
